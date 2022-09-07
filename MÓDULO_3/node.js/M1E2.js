df919c0ec2bd78ae2c6719cd5763c12b76814f37
fexhenrik <99749107+fexhenrik@users.noreply.github.com>
2022-09-07 19:48:43 -0300

    Delete MÓDULO_3/node.js directory

--- MÓDULO_3/node.js/M1E2.js
+++
@@ -1,8 +0,0 @@
-//IDE ESCOLHIDA: VSCode.
-  //A escolha da IDE fora feita com base em sua versatilidade e compatibilidade para rodar diversas linguagens de programação.
-  //A configuração desta fora feita a partir da instalação de extensões como Code Runner e Node JS.//
-
-var x = 3
-var y = 2
-
-console.log('A soma entre x e y tem como resultado o valor: ', x + y)
