0f1cbcb06fa7b5f2ca992f4cf7ffba3f1623d630
fexhenrik <99749107+fexhenrik@users.noreply.github.com>
2022-09-07 20:53:28 -0300

    Create M3E2.js

---
+++ MÓDULO_3/M3E2.js
@@ -0,0 +1,18 @@
+function nome (){
+    return "Felipe";
+}
+
+
+function mult (num1,num2) {
+    return num1 * num2;
+
+}
+
+const idade = (anoatual,anonasc) => {
+    return( (anoatual-anonasc));
+
+}
+
+console.log(nome());
+console.log(mult(2,4));
+console.log(idade(2022-2002));
